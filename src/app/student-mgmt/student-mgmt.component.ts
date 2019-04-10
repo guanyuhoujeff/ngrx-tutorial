e5c79ab66092c7ffb99f9d27fef690e190ddd756
Guan Yu <jeff7522553@gmail.com>
2019-04-11 00:28:06 +0800

    first commit

---
+++ src/app/student-mgmt/student-mgmt.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-student-mgmt',
+  templateUrl: './student-mgmt.component.html',
+  styleUrls: ['./student-mgmt.component.css']
+})
+export class StudentMgmtComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
